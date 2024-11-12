7505dd04c700fb5d5614a6402d9fa1b7e52735aa
justingreco <justin.greco@gmail.com>
2024-11-12 17:12:33 -0500

    deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/raleigh-components-demo/",
   plugins: [react()],
   resolve: {
     alias: [
